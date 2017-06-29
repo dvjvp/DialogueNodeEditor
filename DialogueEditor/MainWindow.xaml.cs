33ec04f6df5157c7268b0fe666a4c5627942d96b
dvjvp <daniel.janowski@666510.com>
2017-06-29 19:01:38 +0200

    Drawing bezier connections

--- DialogueEditor/MainWindow.xaml.cs
+++ DialogueEditor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace DialogueEditor
 {
@@ -49,10 +50,54 @@ namespace DialogueEditor
             Canvas.SetLeft(butt, 210);
             Canvas.SetTop(butt, 210);
 
+            DrawNodeCurve();
+
             //https://forum.unity3d.com/threads/simple-node-editor.189230/
 
             //             drawArea.MouseUp += DrawArea_MouseUp;
             //             drawArea.MouseMove += Rect_MouseMove;
+        }
+
+        void DrawNodeCurve()
+        {
+			//             NodeGraphics.BezierFigure line = new NodeGraphics.BezierFigure();
+			//             line.StartPoint = new Point(Canvas.GetLeft(rect) + rect.ActualWidth / 2, Canvas.GetTop(rect) + rect.ActualHeight);
+			//             line.EndPoint = new Point(Canvas.GetLeft(butt) + rect.ActualWidth / 2, Canvas.GetTop(rect));
+			//             line.StartBezierPoint = new Point(Canvas.GetLeft(rect) + rect.ActualWidth / 2, Canvas.GetTop(rect) + rect.ActualHeight + NodeGraphics.BezierFigure.TangentOffset);
+			//             line.EndBezierPoint = new Point(Canvas.GetLeft(butt) + rect.ActualWidth / 2, Canvas.GetTop(rect) - NodeGraphics.BezierFigure.TangentOffset);
+			// 
+			//             line.Background = new SolidColorBrush(Colors.Black);
+			//             line.BorderBrush = new SolidColorBrush(Colors.Red);
+			// 
+			//             drawArea.Children.Add(line);
+
+			Point from = new Point(Canvas.GetLeft(rect) + rect.Width / 2, Canvas.GetTop(rect) + rect.Height);
+			Point to = new Point(Canvas.GetLeft(butt) + butt.Width / 2, Canvas.GetTop(butt));
+			Point middlePoint = new Point((from.X + to.X) / 2, (from.Y + to.Y) / 2);
+
+			Point[] linePoints = new Point[]
+            {
+                from,
+                new Point(from.X, from.Y + 80),
+				middlePoint
+				//new Point(Canvas.GetLeft(butt) + butt.Width / 2, Canvas.GetTop(butt)),
+                //new Point(90,200), new Point(140,200), new Point(160,200), new Point(180,200), new Point(430,190), new Point(430,280)
+            };
+			Point[] linePoints2 = new Point[]
+			{
+				middlePoint,
+				new Point(to.X, to.Y - 80),
+				to
+			};
+            PolyBezierSegment line = new PolyBezierSegment(linePoints, true);
+			PolyBezierSegment line2 = new PolyBezierSegment(linePoints2, true);
+            PathFigure fig = new PathFigure(from, new PathSegment[] { line, line2 }, false);
+            
+            Path path = new Path();
+            path.Data = new PathGeometry(new PathFigure[] { fig });
+            path.Stroke = new SolidColorBrush(Colors.Black);
+            path.StrokeThickness = 5;
+            drawArea.Children.Add(path);
         }
 
         //         private void Rect_MouseMove(object sender, MouseEventArgs e)
