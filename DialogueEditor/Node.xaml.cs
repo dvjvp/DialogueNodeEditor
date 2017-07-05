c57d6f222651ac4e61bf8f105f4c5c0166538ce0
dvjvp <daniel.janowski@666510.com>
2017-07-05 13:23:55 +0200

    Drag'n'dropping nodes

--- DialogueEditor/Node.xaml.cs
+++ DialogueEditor/Node.xaml.cs
@@ -20,6 +20,8 @@ namespace DialogueEditor
 	/// </summary>
 	public partial class Node : UserControl
 	{
+		protected Point dragOffset;
+
 		public Node()
 		{
 			InitializeComponent();
@@ -31,6 +33,38 @@ namespace DialogueEditor
 		{
 			Canvas.SetLeft(this, x);
 			Canvas.SetTop(this, y);
+		}
+
+		public Point GetPosition()
+		{
+			return new Point(Canvas.GetLeft(this), Canvas.GetTop(this));
+		}
+
+		protected override void OnMouseDown(MouseButtonEventArgs e)
+		{
+			base.OnMouseDown(e);
+			Console.WriteLine("Down");
+			CaptureMouse();
+			MouseMove += OnNodeDragged;
+
+			var mousePos = e.GetPosition((IInputElement)Parent);
+			dragOffset =  GetPosition() - (Vector)mousePos;
+		}
+
+
+		protected override void OnMouseUp(MouseButtonEventArgs e)
+		{
+			base.OnMouseUp(e);
+			Console.WriteLine("Up");
+			ReleaseMouseCapture();
+			MouseMove -= OnNodeDragged;
+		}
+
+		protected void OnNodeDragged(object sender, MouseEventArgs e)
+		{
+			var mousePos = e.GetPosition((IInputElement)Parent);
+			mousePos = mousePos + (Vector)dragOffset;
+			SetPosition(mousePos.X, mousePos.Y);
 		}
 	}
 }
