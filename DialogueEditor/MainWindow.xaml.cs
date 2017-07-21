2f9c76d33a9f197ed774b20e1cc26e009f17c9ca
dvjvp <daniel.janowski@666510.com>
2017-07-21 13:17:28 +0200

    Release: version number upgrade (0.9)

--- DialogueEditor/MainWindow.xaml.cs
+++ DialogueEditor/MainWindow.xaml.cs
@@ -957,7 +957,7 @@ namespace DialogueEditor
 		{
 			MessageBox.Show("\"Woodpecker\" Dialogue editor for Chernobyl Game by The Farm 51.\n"
 				+"Creator: Daniel Janowski\n"
-				+"Version: 0.8.1 Beta\n"
+				+"Version: 0.9 Beta\n"
 				+"Last changes: 19-07-2017"
 				,"Application Info"
 				);
